f5a9bbd594c1b6fdc80179a16124da1e40538cf7
lvjia <lvjia604@pingan.com.cn>
2018-12-21 12:02:06 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import grpcClient from './assets/grpc/client';
 
 Vue.config.productionTip = false;
-
+Vue.prototype.$grpcClient = grpcClient;
 new Vue({
   router,
   store,
